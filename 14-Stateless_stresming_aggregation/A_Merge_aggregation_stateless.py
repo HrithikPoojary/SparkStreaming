b5db86acec8ad2576efe53fb72c212a14b2e3a8b
HrithikPoojary <hrithikpoojary29@gmail.com>
2026-03-20 18:10:39 +0000

    stateless stream

---
+++ 14-Stateless_stresming_aggregation/A_Merge_aggregation_stateless.py
@@ -0,0 +1,151 @@
+def sparkSession():
+    from pyspark.sql import SparkSession 
+    sparksession = SparkSession.builder\
+                                .appName("StreamingAgg")\
+                                .master("local[*]")\
+                                .config("spark.sql.extensions","io.delta.sql.DeltaSparkSessionExtension")\
+                                .config("spark.sql.catalog.spark_catalog","org.apache.spark.sql.delta.catalog.DeltaCatalog")\
+                                .config("spark.jars.packages","io.delta:delta-spark_2.12:3.2.0")\
+                                .config("spark.sql.adaptive.enabled","false")\
+                                .enableHiveSupport()\
+                                .config("spark.sql.warehouse.dir", "/user/hive/warehouse")\
+                                .getOrCreate()
+
+    sparksession.sparkContext.setLogLevel("ERROR")
+
+    return sparksession
+
+spark = sparkSession()
+
+class Brownze:
+    def __init__(self):
+        self.base_dir = None
+        self.spark = None
+
+    def getSchema(self):
+        from pyspark.sql.types import StructType , StructField , IntegerType , StringType , ArrayType , MapType,DoubleType , FloatType
+
+        return (
+                    StructType(
+                                    [
+                                        StructField("InvoiceNumber" , StringType()),
+                                        StructField("CreatedTime" , IntegerType()),
+                                        StructField("StoreID" , StringType()),
+                                        StructField("PosID" , StringType()),
+                                        StructField("CashierID" , StringType()),
+                                        StructField("CustomerType" , StringType()),
+                                        StructField("CustomerCardNo" , StringType()),
+                                        StructField("TotalAmount" , FloatType()),
+                                        StructField("NumberOfItems" , IntegerType()),
+                                        StructField("PaymentMethod" , StringType()),
+                                        StructField("TaxableAmount" , FloatType()),
+                                        StructField("CGST" , FloatType()),
+                                        StructField("SGST" , FloatType()),
+                                        StructField("CESS" , FloatType()),
+                                        StructField("DeliveryType" , StringType()),
+                                        StructField("DeliveryAddress" , StructType(
+                                                                                        [
+                                                                                            StructField("AddressLine" , StringType()),
+                                                                                            StructField("City" , StringType()),
+                                                                                            StructField("State" , StringType()),
+                                                                                            StructField("PinCode" , StringType()),
+                                                                                            StructField("ContactNumber" , StringType())
+                                                                                        ]
+                                                                                    )
+                                                ),
+                                        StructField("InvoiceLineItems" ,  ArrayType( StructType
+                                                                                                (
+                                                                                                  [
+                                                                                                    StructField("ItemCode" , StringType()),
+                                                                                                    StructField("ItemDescription" , StringType()),
+                                                                                                    StructField("ItemPrice" , FloatType()),
+                                                                                                    StructField("ItemQty" , IntegerType()),
+                                                                                                    StructField("TotalValue" , FloatType()) 
+                                                                                                  ]              
+                                                                                                )
+                                                                                    )        
+                                                   )
+                                    ]                        
+                                )
+                 )
+
+    def readInvoices(self ):
+
+        from pyspark.sql.functions import input_file_name
+        return (spark.readStream.format("json")
+                                .schema(self.getSchema())
+                                .load(path = f"/data/test/json/")
+                                .withColumn("inputFile" , input_file_name())
+        )
+
+    def process(self):
+        invoicedf =self.readInvoices()
+        sQuery =  (
+            invoicedf.writeStream.format("delta")
+                                 .queryName("brownze-ingestion")
+                                 .outputMode("append")
+                                 .option("checkpointLocation" , '/tmp/checkpoint_stream_agg_bz')
+                                 .toTable('delta_stream_agg_bz')
+        )
+
+        print("Done With The Brownze")
+        return sQuery
+
+
+class Gold():
+    def __init__(self):
+        self.base_dir = None
+
+    def readBronze(self):
+        return (
+            spark.readStream.table('delta_stream_agg_bz')
+            )
+        
+    def getAggregates(self,readbrowndf):
+        from pyspark.sql.functions import sum,expr 
+        return (
+            readbrowndf.groupBy("CustomerCardNo")
+                            .agg(sum("TotalAmount").alias("Totalamount"),
+                                  (sum("TotalAmount") * 0.02).alias("TotalPoints"))
+        )
+
+    def aggregate_upsert(self , invoices_df , batchid):
+
+        aggregate_df = self.getAggregates(invoices_df)
+        aggregate_df.createOrReplaceTempView("source_batch")
+        merge_statement = """
+                        merge into stateless_agg t
+                        using source_batch s
+                        on t.CustomerCardNo = s.CustomerCardNo
+                        when matched then
+                        update set t.TotalAmount = s.TotalAmount + t.TotalAmount ,
+                                   t.TotalPoints = s.TotalPoints + t.TotalPoints
+                        when not matched then
+                        insert *
+        """
+        batchdf.sparkSession.sql(merge_statement) 
+
+    def saveResult(self,invoices_df):
+        return (
+            invoices_df.writeStream.format("delta")
+                                  .queryName("gold-integration")
+                                  .outputMode("update")
+                                  .foreachBatch(self.aggregate_upsert)
+                                  .option("checkpointLocation" , '/tmp/checkpoint_stream_agg_gl')
+                                  .start()
+        )
+        
+    def process(self):
+        invoices_df = self.readBronze()
+        #aggregate_df = self.getAggregates(invoices_df)
+        sQuery = self.saveResult(invoices_df)
+        print("Done With The Gold")
+        return sQuery
+
+
+
+    
+
+
+
+    
---
+++ 14-Stateless_stresming_aggregation/B_merge_aggregation_stateless_test_suite.py
@@ -0,0 +1,80 @@
+class StreamingAggTestSuite:
+    def __init__(self):
+        self.base_dir = None
+        self.spark = None
+
+    def cleanUP(self):
+        print("Clean Up has started..............")
+        import subprocess
+        subprocess.run(['hadoop' , 'fs' ,"-rm" , "-r" , "/data/test/json/*"])
+        subprocess.run(['hadoop' , 'fs' , "-rm" , "-r" , f"/tmp/checkpoint_stream_agg_bz/*"])
+        subprocess.run(['hadoop' , 'fs' , "-rm" , "-r" , f"/tmp/checkpoint_stream_agg_gl/*"])
+        subprocess.run(['hadoop' , 'fs' , "-rm" , "-r" , f"/user/hive/warehouse/delta_stream_agg_b*"])
+        subprocess.run("bash -c 'rm -rf /home/hrithik_poojary/Spark_Streaming/SparkStreaming/13_Straming_update/spark-warehouse/{*,.*}'", shell = True)
+        print("Clean Up is completed...............")
+
+
+    def ingestion(self,itr):
+        import subprocess
+        subprocess.run(['hadoop' , 'fs' , "-cp" , f'/data/json/invoices-{itr}.json' , f"/data/test/json/"])
+        print(f"Ingestion is completed for the {itr}")
+
+    def waitforMicroBatch(self ,sleep=60):
+        print("Waiting for 30 seconds")
+        import time 
+        time.sleep(sleep)
+        print("Completed 30 Seconds.................")
+
+    def assertResultBrownze(self , expected_result):
+
+        actual_result = self.spark.sql("select count(*) from delta_stream_agg_bz").collect()[0][0]
+        assert expected_result == actual_result , f"Failed Due to Mismatch Actual - {actual_result}  Expected - {expected_result}"
+
+    def assertResultGold(self , expected_result):
+        
+        actual_result = self.spark.sql("select totalAmount from delta_stream_agg_gl1 where CustomerCardNo=='2262471989'").collect()[0][0]
+
+        assert expected_result == actual_result , f"Failed Due to Mismatch Actual - {actual_result}  Expected - {expected_result}"
+
+
+    def runtestCases(self):
+        from A_update_stream import Brownze , Gold ,spark 
+        self.cleanUP()
+        spark.sql("create table if not exists stateless_agg (CustomerCardNo string , TotalAmount int , TotalPoints float ) USING DELTA")
+
+        bz = Brownze()
+        bzQuery = bz.process()
+        self.spark = spark
+
+        gl = Gold()
+        glQuuery = gl.process()
+
+
+        print("First Test Case Running............")
+        self.ingestion(1)
+        self.waitforMicroBatch()
+        self.assertResultBrownze(501)
+        self.assertResultGold(36859)
+        print("First Test Case Passeddddddd............")
+
+
+        print("Second Test Case Running............")
+        self.ingestion(2)
+        self.waitforMicroBatch()
+        self.assertResultBrownze(501+500)
+        self.assertResultGold(36859+20740)
+        print("Second Test Case Passeddddddd............")
+
+        print("Third Test Case Running............")
+        self.ingestion(3)
+        self.waitforMicroBatch()
+        self.assertResultBrownze(501+500+590)
+        self.assertResultGold(36859+20740+31959)
+        print("Third Test Case Passeddddddd............")
+
+        bzQuery.stop()
+        glQuuery.stop()
+
+if __name__ == '__main__':
+    sats = StreamingAggTestSuite()
+    sats.runtestCases()
