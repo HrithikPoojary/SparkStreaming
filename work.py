3fcc6a2433e061870a8a0884dee87fd839df1116
HrithikPoojary <hrithikpoojary29@gmail.com>
2026-03-23 18:18:30 +0000

    merge statement

--- work.py
+++ work.py
@@ -9,3 +9,11 @@ dbutils.fs.help("cp")
 dbutils.fs.help("head")
 
 %sql
+
+merge target_table i
+using source_table s 
+on i.id = s.id 
+when matched then
+        update
+when not matched then
+        insert
